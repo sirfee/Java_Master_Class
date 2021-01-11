566af1304c35aaaf466d0efa039506e346818564
Aaron Thiele <56663193+sirfee@users.noreply.github.com>
2021-01-11 13:44:08 +1300

    PrimitiveTypesChallange

---
+++ PrimitiveTypesChallange/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte myNewByte = 2;
+        short myNewShort = 365;
+        int myNewInt = 551235;
+        long myNewLong = 50000 + (10L * (myNewByte + myNewShort + myNewInt));
+        System.out.println("My new value = " + myNewLong);     
+        
+        short shotTotal = (short)(100 + 10 * 
+                (myNewByte + myNewShort + myNewInt));
+    }
+}
