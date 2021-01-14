2bbd61ebaf5ce6023c52bdc78f4274cdf9bfb382
Aaron Thiele <56663193+sirfee@users.noreply.github.com>
2021-01-15 12:46:24 +1300

    CharBooleanandStrings

---
+++ CharAndBoolean/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar); 
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar); 
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        
+        boolean isCustomerOverTwentyOne = true;
+        
+    }
+}
