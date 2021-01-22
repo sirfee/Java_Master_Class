dc8eb33330de616fceb28ec00069e5c418ec4b08
Aaron Thiele <56663193+sirfee@users.noreply.github.com>
2021-01-22 16:22:20 +1300

    Update App.java
    End of first steps

--- Operators/src/App.java
+++ Operators/src/App.java
@@ -38,5 +38,56 @@ public class App {
         result -=2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (isAlien == false){
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("You got the highscore");
+        }
+
+        int secoundTopScore = 60;
+        if ((topScore > secoundTopScore) && (topScore < 100)){
+            System.out.println("Greater than secound top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secoundTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar){
+            System.out.println("wasCar is true");
+        } 
+
+        double myVariable = 20.00d;
+        double mySecoundVariable = 80.00d;
+        double myResult = (myVariable + mySecoundVariable) * 100.00d;
+        System.out.println("result = " + myResult);
+        double remainder = myResult % 40.00;
+
+        boolean hasRemainder = (remainder != 0.00) ? true: false;
+        System.out.println("hasRemainder = " + hasRemainder);
+
+        if (hasRemainder){
+            System.out.println("Got some remainder");
+        }else{
+            System.out.println("Has no remainder");
+        }
+
     }
 }
